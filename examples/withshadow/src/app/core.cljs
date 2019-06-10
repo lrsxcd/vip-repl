ba0529665c2307c22eea8904a6004f5edb7d6bca
david <davidvoler@gmail.com>
2019-06-10 13:35:19 +0300

    adding marker

--- examples/withshadow/src/app/core.cljs
+++ examples/withshadow/src/app/core.cljs
@@ -1,16 +1,19 @@
 (ns app.core
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
-            ["react-leaflet" :refer [Map TileLayer]]))
+            ["react-leaflet" :refer [Map TileLayer  Marker Popup]]))
 
 (defn app
   []
   [:div "VIP-REPL"]
-  
-  [:> Map  {:center [ 51.505 -0.09 ]
-            :zoom 13} [:> TileLayer
-                       {:attribution= "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                        :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]])
+
+  [:> Map  {:center [51.505 -0.09]
+            :zoom 13}
+   [:> TileLayer
+    {:attribution= "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+     :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]
+   [:> Marker {:position [51.505 -0.09]}
+    [:> Popup "A pretty CSS3 popup."]]])
 
 (defn ^:dev/after-load start
   []
