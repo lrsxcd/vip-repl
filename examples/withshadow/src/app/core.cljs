3a7240eb810b4569a4c358674bf735ef5abd9a34
david <davidvoler@gmail.com>
2019-06-10 12:12:37 +0300

    map with center

--- examples/withshadow/src/app/core.cljs
+++ examples/withshadow/src/app/core.cljs
@@ -1,14 +1,15 @@
 (ns app.core
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
-            ["react-leaflet" :refer [Map]]
-            ))
+            ["react-leaflet" :refer [Map TileLayer]]))
 
 (defn app
   []
   [:div "VIP-REPL"]
-  [:> Map]
-)
+  
+  [:> Map  {:center [ 51.505 -0.09 ]
+            :zoom 13} [:> TileLayer
+                       {:url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]])
 
 (defn ^:dev/after-load start
   []
