e263af2183afc85bcad2c85a0e38e25e2b7c8249
Amit Ramon <amit@riseup.net>
2019-06-17 20:18:32 +0300

    Added function that create markers and add them to the map

--- examples/withshadow/src/app/core.cljs
+++ examples/withshadow/src/app/core.cljs
@@ -3,13 +3,18 @@
             [re-frame.core :as rf]
             ; [app.vip-repl-api :refer [create-map]]
             [app.leaflet-api-david :refer [create-map]]
-            ["react-leaflet" :refer [Map TileLayer  Marker Popup]]))
+            [react-leaflet :refer [Map TileLayer  Marker Popup]]))
 
 
 (defn app
   []
-  [:div "VIP-REPL" [:input {:type "range" :id "start" :name "test" :min "0" :max "11"}]]
-  [create-map  51.505 -0.09  13 "" ""])
+  [:div "VIP-REPL" [:input {:type "range" :id "start"
+                            :name "test" :min "0" :max "11"}]]
+  [create-map 59.92 10.75 12 nil nil])
+
+;;  [create-map  51.505 -0.09  13 "" ""]
+
+
   ; [:> Map  {:center [51.505 -0.09]
   ;           :zoom 13}
   ;  [:> TileLayer  {:attribution= "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
