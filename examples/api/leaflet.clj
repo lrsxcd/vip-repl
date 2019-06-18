bdfa08e7dcf09cc50d2fa1b305ec6974d91aa058
david <davidvoler@gmail.com>
2019-06-18 11:22:28 +0300

    api examples

--- examples/api/leaflet.clj
+++ examples/api/leaflet.clj
@@ -1,13 +1,13 @@
 (ns api.leaflet)
 
 
-(defn create-context [name]
-  {:leaflet nil})
+(defn create-context [name])
+(defn get-default-context [])
 
 (defn leaflet-map-event [leaflet event-id action])
 
 (defn leaflet-map [context  center zoom data_path events]
-  {:leaflet})
+  )
 (defn historgram[context data_path])
 
 (defn piechart [context ])
